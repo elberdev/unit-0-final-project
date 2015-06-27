7f145a56601cb1d3a090442c4179a7075b27b4b2
elberdev <elberdev@gmail.com>
2015-06-27 12:48:55 -0400

    re-wrote item class

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,10 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+@interface listItem : NSObject
+@end
+
+@implementation listItem {
+    NSString *_itemDescription;
+    int _priority;
+    BOOL _doneStatus;
+}
+
+-(id)init {
+    if(self = [super init]) {
+        _priority = 1;
+        _doneStatus = NO;
+    }
+    return self;
+}
+
+-(void)setItemDescription:(NSString *)itemDescription {
+    _itemDescription = itemDescription;
+}
+
+-(NSString *)itemDescription {
+    return _itemDescription;
+}
+
+-(void)setPriority:(int)priority {
+    _priority = priority;
+}
+
+-(int)priority {
+    return _priority;
+}
+
+-(void)setDoneStatus:(BOOL)doneStatus {
+    _doneStatus = doneStatus;
+}
+
+-(BOOL)doneStatus {
+    return _doneStatus;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"whimmy wam wam wozzle");
+        
+        
     }
     return 0;
 }
